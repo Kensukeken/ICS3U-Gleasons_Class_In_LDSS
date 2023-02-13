ee7f407aa4fe6b93a1f612d0d593b986961dd867
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-13 17:43:27 -0500

    Create variables1.cpp

---
+++ Introduction to variables/variables1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+  //Declare some variables
+  //Name it's type, then give it a name
+  int num1;
+  double num2;
+  char initial;
+  bool myBooTeame;
+
+  //Declare variables and assign value to those variables
+  num1 = 12;
+  num2 = 0.75;
+  initial = 'D';
+  myBooTeame = true; 
+  num1 = 100;
+
+  double pi = 3.14;
+  int health = 10;
+  char symbol = 'j';
+
+  //Use your variables
+  //Print all your variables to the console
+  std::cout << num1 << std::endl;
+  std::cout << num2 << std::endl;
+  std::cout << initial << std::endl;
+  std::cout << myBooTeame << std::endl;
+  std::cout << pi << std::endl;
+  std::cout << health << std::endl;
+  std::cout << symbol << std::endl;
+
+  return 0;
+}
