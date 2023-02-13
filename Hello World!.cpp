a0365b9f169066e41331a1224667fec0611af8e6
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-13 17:35:25 -0500

    Create Hello World!.cpp

---
+++ Hello World!.cpp
@@ -0,0 +1,14 @@
+// Hello World
+// This is comment
+// This is multiple
+
+#include <iostream>
+
+int main() {  
+  std :: cout << "02/08/2023\n";
+	std :: cout << "Hello World!\n";	
+  std :: cout << " My name is (Blank).";
+  std :: cout << " I am (age) years old";
+  std :: cout << "\n"; //This use blank space
+
+}
