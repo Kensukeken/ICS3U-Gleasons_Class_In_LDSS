adb25e19fa94b71cbecb40942315e16ace1f1f31
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-13 17:51:11 -0500

    Create taxcalc.cpp

---
+++ Math Operators/taxcalc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+std::cout << "This is a Tax Calculator!\n";
+double price = 0.95;
+double tax = 1.1;
+
+std::cout << "The original price is: " << price << "\n";
+std::cout << "The tax rate is: " << tax << "\n";
+
+  
+}
