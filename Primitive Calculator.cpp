f867df84e830fb354e1b22b91fffcf7fd34f3c28
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-15 16:32:45 -0500

    Create Primitive Calculator.cpp

---
+++ Primitive Calculator.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+
+    printf("Enter the first number:\n");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number:\n");
+    scanf("%lf", &num2);
+
+    printf("calculating...\n");
+    printf("%.2lf+%.2lf=%.2lf\n", num1, num2, num1 + num2);
+    printf("%.2lf-%.2lf=%.2lf\n", num1, num2, num1 - num2);
+    printf("%.2lfx%.2lf=%.2lf\n", num1, num2, num1 * num2);
+    printf("%.2lf/%.2lf=%.2lf\n", num1, num2, num1 / num2);
+
+}
