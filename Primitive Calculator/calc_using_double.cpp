64438230eb62f555135de90f2dae076d1747aee0
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-15 17:33:39 -0500

    Update calc_using_double.cpp

--- Primitive Calculator/calc_using_double.cpp
+++ Primitive Calculator/calc_using_double.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  printf("Welcome to Calculator\n");
-    double num1, num2;
+    printf("Welcome to Calculator\n");
+    double num1, num2; // use double instead of int for decimal values
 
     printf("Enter the first number:\n");
     scanf("%lf", &num1);
@@ -11,9 +11,10 @@ int main() {
     scanf("%lf", &num2);
 
     printf("Calculating...\n");
-    printf("%.2lf+%.2lf=%.2lf\n", num1, num2, num1 + num2);
-    printf("%.2lf-%.2lf=%.2lf\n", num1, num2, num1 - num2);
-    printf("%.2lfx%.2lf=%.2lf\n", num1, num2, num1 * num2);
-    printf("%.2lf/%.2lf=%.2lf\n", num1, num2, num1 / num2);
+    printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+    printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+    printf("%.2lf x %.2lf = %.2lf\n", num1, num2, num1 * num2);
+    printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
 
+    return 0; // indicate successful program execution
 }
