db6fe30f129d5c71c2d863c03cf806ed345cf316
Kokichi <85637598+Kensukeken@users.noreply.github.com>
2023-02-15 21:33:37 -0500

    Update helloword.cpp

--- Hello World!/helloword.cpp
+++ Hello World!/helloword.cpp
@@ -6,9 +6,14 @@
 
 int main() {  
   std :: cout << "02/08/2023\n";
+      // Output a string to the console
 	std :: cout << "Hello World!\n";	
   std :: cout << " My name is (Blank).";
   std :: cout << " I am (age) years old";
   std :: cout << "\n"; //This use blank space
-
+    int x = 5; // Declare and initialize an integer variable
+    
+    // Output the value of x to the console
+    std::cout << "The value of x is: " << x << "\n";
+  
 }
