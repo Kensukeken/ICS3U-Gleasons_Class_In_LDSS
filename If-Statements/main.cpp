ce0ffdfddcdeae285a9f6df8dcbc9da2779f7552
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-02-27 09:13:47 -0500

    Create main.cpp

---
+++ If-Statements/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+	int num1 = 10;
+	int num2 = 5;
+
+	if(num1 > num2){
+		printf("num 1 is greater than num2 \n");
+	}
+
+	if(num2 > num1){
+		printf("num2 is greater than num1 \n");
+	}
+
+	if(num1 == num2){
+		printf("num1 is equal to num2 \n");
+	}
+
+
+	printf("Done \n");
+	
+}
