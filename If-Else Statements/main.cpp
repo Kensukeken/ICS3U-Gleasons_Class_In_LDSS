349ad485a4913e9725b2e5cd69616519d27526fb
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-03-01 08:44:30 -0500

    Create main.cpp

---
+++ If-Else Statements/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+	//Temp Program
+	printf("Please enter a temperature. ");
+	int temp;
+	scanf("%d", &temp);
+
+	if(temp > 32){
+		printf("It is hot. \n");
+	
+  } else if (temp < 4){
+		printf("It is cold. \n");
+	}
+
+	printf("\n\n");
+
+	//Positive, Negative, Zero Program
+	printf("Please enter a number. ");
+	int num;
+	scanf("%d", &num);
+
+	
+	
+	printf("\n\n");
+
+	//Voting Program
+	printf("Please enter your age. ");
+	int age;
+	scanf("%d", &age);
+
+	
+}
