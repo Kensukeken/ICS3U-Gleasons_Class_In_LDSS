a2363a3728ffe822dbb9b52af6e375ac77428b02
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-03-01 08:59:13 -0500

    Update main.cpp

--- If-Else Statements/main.cpp
+++ If-Else Statements/main.cpp
@@ -7,21 +7,27 @@ int main() {
 	int temp;
 	scanf("%d", &temp);
 
-	if(temp > 32){
-		printf("It is hot. \n");
-	
-  } else if (temp < 4){
-		printf("It is cold. \n");
-	}
-
+	if(temp > 32){ // if this condition is true,
+		printf("It is hot. \n"); // execute this, otherwise
+  } else if (temp < 4){ //if this condition is true,
+		printf("It is cold. \n"); // excute this
+	} else { // or else
+    printf("It is a perfect day.\n"); //execute this
+  }
+  
 	printf("\n\n");
 
 	//Positive, Negative, Zero Program
 	printf("Please enter a number. ");
 	int num;
 	scanf("%d", &num);
-
-	
+  if(num >0){
+    printf("Positive\n");
+  } else if(num < 0){
+    printf("Negative\n");
+  } else if(num == 0){
+    printf("Zero\n");
+  }
 	
 	printf("\n\n");
 
@@ -30,5 +36,9 @@ int main() {
 	int age;
 	scanf("%d", &age);
 
-	
+  if(age >= 18){
+    printf("You can vote!");
+  } else{
+    printf("You can not vote.");
+  }
 }
