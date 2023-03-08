d5ffee5035d66afb9f32c4440c9be313fcca298d
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-03-08 09:13:19 -0500

    Create main.cpp

---
+++ New Collector/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+  char name1[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name1);
+
+  char name2[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name2);
+  
+  char name3[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name3);
+
+  char name4[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name4);
+
+  char name5[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name5);
+
+  char name6[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name6);
+
+  char name7[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name7);
+  
+  char name8[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name8);
+
+  char name9[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name9);
+
+  char name10[50];
+	printf("Please enter a name: \n");
+	scanf("%s", name10);
+  
+}
